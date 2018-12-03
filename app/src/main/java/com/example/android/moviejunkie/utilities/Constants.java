a997ef3af99ee4cacda73b0c63da1e5653587a8d
amk87 <adamkirsch1@gmail.com>
2018-12-03 22:43:44 +0200

    api key removed

--- app/src/main/java/com/example/android/moviejunkie/utilities/Constants.java
+++ app/src/main/java/com/example/android/moviejunkie/utilities/Constants.java
@@ -3,7 +3,7 @@ package com.example.android.moviejunkie.utilities;
 public class Constants {
 
     // initialize String constant to store value of private api key the the Movie DB Api
-    public static final String API_KEY = "5f64b72ee2d1533149e6ab6aa25a35bb";
+    public static final String API_KEY = "";
 
     // constants for intent extra keys
     public final static String MOVIE_OBJECT_KEY = "MOVIE_OBJECT";
